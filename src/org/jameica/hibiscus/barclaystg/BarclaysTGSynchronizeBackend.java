a77eab7d91404c3e2ba5e563d0c6c0c710e62168
Sven Bursch-Osewold <git@bursch.com>
2014-12-08 13:09:58 +0100

    Neues Interface

--- src/org/jameica/hibiscus/barclaystg/BarclaysTGSynchronizeBackend.java
+++ src/org/jameica/hibiscus/barclaystg/BarclaysTGSynchronizeBackend.java
@@ -19,7 +19,7 @@ import de.willuhn.util.ProgressMonitor;
  * Implementierung eines Sync-Backends.
  */
 @Lifecycle(Type.CONTEXT)
-public class BarclaysTGSynchronizeBackend extends AbstractSynchronizeBackend
+public class BarclaysTGSynchronizeBackend extends AbstractSynchronizeBackend<BarclaysTGSynchronizeJobProvider>
 {
   private final static I18N i18n = Application.getPluginLoader().getPlugin(Plugin.class).getResources().getI18N();
 
@@ -38,7 +38,7 @@ public class BarclaysTGSynchronizeBackend extends AbstractSynchronizeBackend
    * @see de.willuhn.jameica.hbci.synchronize.AbstractSynchronizeBackend#getJobProviderInterface()
    */
   @Override
-  protected Class<? extends SynchronizeJobProvider> getJobProviderInterface()
+  protected Class<BarclaysTGSynchronizeJobProvider> getJobProviderInterface()
   {
     return BarclaysTGSynchronizeJobProvider.class;
   }
